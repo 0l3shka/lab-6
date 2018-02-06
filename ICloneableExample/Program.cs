fd6cf7698712b3736d5a6e50cb81250f97ccf015
0l3shka <q0l3shka@gmail.com>
2018-02-06 15:32:32 +0700

    +main

--- ICloneableExample/Program.cs
+++ ICloneableExample/Program.cs
@@ -10,6 +10,22 @@ namespace ICloneableExample
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("****** A First Look at Interfaces ******\n");
+
+            string myStr = "Hello";
+            OperatingSystem unixOS = new OperatingSystem(PlatformID.Unix, new Version());
+            System.Data.SqlClient.SqlConnection sqlCnn = new System.Data.SqlClient.SqlConnection();
+            CloneMe(myStr);
+            CloneMe(unixOS);
+            CloneMe(sqlCnn);
+            Console.ReadLine();
+        }
+
+        private static void CloneMe(ICloneable c)
+        {
+            object theClone = c.Clone();
+            Console.WriteLine("Your clone is a: {0}", theClone.GetType().Name);
+
         }
     }
 }
