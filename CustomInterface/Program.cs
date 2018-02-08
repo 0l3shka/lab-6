5cdebad9c2224ac5a2bbacf4951e062e11fd15d6
0l3shka <q0l3shka@gmail.com>
2018-02-08 15:47:37 +0700

    without try/catch

--- CustomInterface/Program.cs
+++ CustomInterface/Program.cs
@@ -10,17 +10,14 @@ namespace CustomInterface
     {
         static void Main(string[] args)
         {
-            Circle c = new Circle("Lisa");
-            IPointy itfPt = null;
-            try
-            {
-                itfPt = (IPointy)c;
-                Console.WriteLine(itfPt.Points);
-            }
-            catch (InvalidCastException e)
-            {
-                Console.WriteLine(e.Message);
-            }
+            Hexagon hex2 = new Hexagon("Peter");
+            IPointy itfPt2 = hex2 as IPointy;
+
+            if (itfPt2 != null)
+                Console.WriteLine("Points: {0}", itfPt2.Points);
+            else
+                Console.WriteLine("OOPS! Not pointy...");
+
             Console.ReadLine();
         }
     }
