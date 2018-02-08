ca45cf84f7b60aec5621cf39c06945926be3469a
0l3shka <q0l3shka@gmail.com>
2018-02-08 15:25:58 +0700

    Create new project CustomInterface, import Shape class

---
+++ CustomInterface/Shape.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shapes
+{
+    abstract class Shape
+    {
+        public Shape(string name="NoName")
+        { PetName = name; }
+
+        public string PetName { get; set; }
+
+        public abstract void Draw();
+        
+    }
+
+    class Circle : Shape
+    {
+        public Circle() { }
+        public Circle(string name) : base(name) { }
+        public override void Draw()
+        {
+            Console.WriteLine("Drawing {0} the Circle", PetName);
+        }
+    }
+
+    class Hexagon : Shape
+    {
+        public Hexagon() { }
+        public Hexagon(string name) : base(name) { }
+        public override void Draw()
+        {
+            Console.WriteLine("Drawing {0} the Gexagon", PetName);
+        }
+    }
+    
+}
