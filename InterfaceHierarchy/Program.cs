3e40688186f39a922c51ee09d2d76fd1fdb558a5
0l3shka <q0l3shka@gmail.com>
2018-02-08 16:36:59 +0700

    Test interface hierarchy

--- InterfaceHierarchy/Program.cs
+++ InterfaceHierarchy/Program.cs
@@ -6,10 +6,37 @@ using System.Threading.Tasks;
 
 namespace InterfaceHierarchy
 {
+    public class BitmapImage : IAdvancedDraw
+    {
+        public void Draw()
+        {
+            Console.WriteLine("Drawing...");
+        }
+
+        public void DrawInBoundingBox(int top,int left,int bottom,int right)
+        {
+            Console.WriteLine("Drawing in a box...");
+        }
+        public void DrawUpsideDown()
+        {
+            Console.WriteLine("Drawing upside down!");
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("****** Simple Interface Hierarchy ******");
+            BitmapImage myBitmap = new BitmapImage();
+            myBitmap.Draw();
+            myBitmap.DrawInBoundingBox(10, 10, 100, 150); 
+            myBitmap.DrawUpsideDown();
+
+            IAdvancedDraw iAdvDraw = myBitmap as IAdvancedDraw;
+            if (iAdvDraw != null)
+                iAdvDraw.DrawUpsideDown();
+
+            Console.ReadLine();
         }
     }
 }
