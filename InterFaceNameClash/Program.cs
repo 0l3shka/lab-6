2120d2b048b41e9134f07c2f40e1368d9621b7f1
0l3shka <q0l3shka@gmail.com>
2018-02-08 16:23:21 +0700

    test interface

--- InterFaceNameClash/Program.cs
+++ InterFaceNameClash/Program.cs
@@ -6,10 +6,31 @@ using System.Threading.Tasks;
 
 namespace InterFaceNameClash
 {
+    class Octagon: IDrawToForm, IDrawToMemory, IDrawToPrinter
+    {
+        public void Draw()
+        {
+            Console.WriteLine("Drawing the Octagon...");
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("***** Fun with Interface Name Clashes *****\n");
+            Octagon oct = new Octagon();
+
+            IDrawToForm itfForm = (IDrawToForm)oct;
+            itfForm.Draw();
+
+            IDrawToPrinter itfPrinter = (IDrawToPrinter)oct;
+            itfPrinter.Draw();
+
+            IDrawToMemory itfMemory = (IDrawToMemory)oct;
+            itfMemory.Draw();
+
+            Console.ReadLine();
+
         }
     }
 }
